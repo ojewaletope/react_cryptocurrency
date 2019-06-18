3c31638eb0de5093f4bb8ac483d3c878769e07b1
Tope Ojewale <topeojewale@gmail.com>
2019-06-18 08:03:32 +0100

    enabled search and render search results

--- src/components/common/search/Search.js
+++ src/components/common/search/Search.js
@@ -7,35 +7,73 @@ import Loading            from "../Loading";
 class Search extends Component{
   state = {
     searchQuery: '',
-    currencies: [],
+    searchResults: [],
     error: null,
     loading: false
   };
   handleInputChange(e) {
-    this.setState({[e.target.name]: e.target.value})
-    if (!this.state.searchQuery) {
+  const searchQuery = e.target.value;
+    if (!searchQuery) {
       return ''
     }
     this.setState({loading: true});
-    fetch(`${API_URL}/autocomplete?searchQuery=${this.state.searchQuery}`).then(responseHandler).then(data => {
-     this.setState({loading: false, currencies: data})
+    fetch(`${API_URL}/autocomplete?searchQuery=${searchQuery}`).then(responseHandler).then(data => {
+     this.setState({loading: false, searchResults: data})
     }).catch(err => {
       this.setState({
-        error: err.errorMessage
+        error: err.errorMessage,
+        loading: false
       })
     })
   }
+  renderSearchResults() {
+    const { searchResults, searchQuery, loading } = this.state;
+
+    if (!searchQuery) {
+      return '';
+    }
+
+    if (searchResults.length > 0) {
+      return (
+        <div className="Search-result-container">
+          {searchResults.map(result =>
+            <div
+              key={result.id}
+              className="Search-result"
+            >
+              {result.name} ({result.symbol})
+            </div>
+          )}
+        </div>
+      )
+    }
+
+    // Send no result, only if loading is set to false
+    // To avoid showing no result, when actually there are ones
+    if (!loading) {
+      return (
+        <div className="Search-result-container">
+          <div className="Search-no-result">
+            No results found.
+          </div>
+        </div>
+      )
+    }
+  }
   render() {
-    const {loading, currencies, error} = this.state
+    const {loading} = this.state;
     return (
       <div className="Search">
-        <span className="Search-icon"/>
-          <input type="text" name="searchQuery" className="Search-input" placeholder="Currency name" onChange={(e) => this.handleInputChange(e)}/>
-        {loading &&
-        <div className="Search-loading">
-          <Loading width="12px" height="12px"/>
+        <div>
+          <span className="Search-icon"/>
+          <input type="text" className="Search-input" placeholder="Currency name" onChange={(e) => this.handleInputChange(e)}/>
+          {loading &&
+          <div className="Search-loading">
+            <Loading width="12px" height="12px"/>
+          </div>
+          }
         </div>
-        }
+        {this.renderSearchResults()}
       </div>
     )
   }
